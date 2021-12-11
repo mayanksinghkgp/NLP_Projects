a81844b8172f58238e9025cff78e56e124bfb71b
mayanksinghkgp <90528116+mayanksinghkgp@users.noreply.github.com>
2021-12-11 14:21:51 +0530

    Interface Creation

--- Chatbot/chatbot.py
+++ Chatbot/chatbot.py
@@ -67,3 +67,42 @@ def chatbot_response(text):
 
 #%%
 #developing the gui
+import tkinter
+from tkinter import *
+
+
+def send():
+    msg = EntryBox.get("1.0",'end-1c').strip()  #line 1 character 0 to end -1c for \n
+    EntryBox.delete("0.0",END)
+    if msg != '':
+        ChatLog.config(state=NORMAL)
+        ChatLog.insert(END, "You: " + msg + '\n\n')
+        ChatLog.config(foreground="#442265", font=("Helvetica", 12 ))
+        res = chatbot_response(msg)
+        ChatLog.insert(END, "Bot: " + res + '\n\n')
+        ChatLog.config(state=DISABLED)
+        ChatLog.yview(END)
+
+
+base = Tk()      #window start
+base.title("I am a chatbot short and stout!")
+base.geometry("400x500")
+base.resizable(width=FALSE, height=FALSE)
+#Create Chat window
+ChatLog = Text(base, bd=0, bg="#C1CDCD", height="8", width="50", font="Helvetica",)
+ChatLog.config(state=DISABLED)
+#Bind scrollbar to Chat window
+scrollbar = Scrollbar(base, command=ChatLog.yview, cursor="spider")
+ChatLog['yscrollcommand'] = scrollbar.set
+#Create Button to send message
+SendButton = Button(base, font=("Helvetica",12,'bold'), text="Send", width="12", height=5,
+                    bd=0, bg="#008B8B", activebackground="#A2CD5A",fg='#ffffff',
+                    command= send )
+#Create the box to enter message
+EntryBox = Text(base, bd=0, bg="white",width="29", height="5", font="Helvetica")
+#Place all components on the screen
+scrollbar.place(x=376,y=6, height=386)
+ChatLog.place(x=6,y=6, height=386, width=370)
+EntryBox.place(x=128, y=401, height=90, width=265)
+SendButton.place(x=6, y=401, height=90)
+base.mainloop()    #window end
